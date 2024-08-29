9116838af1dd1eb7116397b662c9bab78b213724
Trí-Anh Nguyễn <78487345+AubrynnReina@users.noreply.github.com>
2024-08-29 16:49:18 +0700

    Update restaurant-growth.sql

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,31 +1,31 @@
--- -- My solution: I handled this problem before
--- -- I gave up, now I'm back with some knowledge of Window functions 
--- -- and picking out rows for partitions
--- -- Not likely an optimal solution, but I'm proud that I got it this time
--- -- 239ms, 30%
--- SELECT 
---     visited_on,
+-- My solution: I handled this problem before
+-- I gave up, now I'm back with some knowledge of Window functions 
+-- and picking out rows for partitions
+-- Not likely an optimal solution, but I'm proud that I got it this time
+-- 239ms, 30%
+SELECT 
+    visited_on,
 
---     SUM(total_per_day) OVER(
---         ORDER BY visited_on
---         ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
---     ) AS amount,
+    SUM(total_per_day) OVER(
+        ORDER BY visited_on
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
 
---     ROUND(
---         AVG(total_per_day) OVER(
---             ORDER BY visited_on
---             ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
---         )
---     , 2) AS average_amount
+    ROUND(
+        AVG(total_per_day) OVER(
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        )
+    , 2) AS average_amount
 
--- FROM (
---     SELECT
---         visited_on,
---         SUM(amount) as total_per_day
---     FROM customer
---     GROUP BY visited_on
--- )
--- OFFSET 6
+FROM (
+    SELECT
+        visited_on,
+        SUM(amount) as total_per_day
+    FROM customer
+    GROUP BY visited_on
+)
+OFFSET 6
 
 -- Another solution: 
 -- This one doesn't even need a window function
