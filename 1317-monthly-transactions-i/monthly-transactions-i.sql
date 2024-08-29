e646a02ec737de88f4a826f4a857239d554b8b99
Trí-Anh Nguyễn <78487345+AubrynnReina@users.noreply.github.com>
2024-08-29 15:50:51 +0700

    Update monthly-transactions-i.sql

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,23 +1,23 @@
--- -- My solution (Used CASE WHEN as COUNTIF, SUMIF)
--- SELECT
---     TO_CHAR(trans_date, 'YYYY-MM') AS "month",
---     -- month is a keyword, should be in quotes for clarity
---     country,
---     COUNT(*) AS trans_count,
---     SUM(CASE 
---         WHEN state = 'approved' 
---         THEN 1 
---         ELSE 0 END
---     ) AS approved_count,
+-- My solution (Used CASE WHEN as COUNTIF, SUMIF)
+SELECT
+    TO_CHAR(trans_date, 'YYYY-MM') AS "month",
+    -- month is a keyword, should be in quotes for clarity
+    country,
+    COUNT(*) AS trans_count,
+    SUM(CASE 
+        WHEN state = 'approved' 
+        THEN 1 
+        ELSE 0 END
+    ) AS approved_count,
 
---     SUM(amount) AS trans_total_amount,
---     SUM(CASE 
---         WHEN state = 'approved'
---         THEN amount
---         ELSE 0 END) AS approved_total_amount
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE 
+        WHEN state = 'approved'
+        THEN amount
+        ELSE 0 END) AS approved_total_amount
 
--- FROM transactions
--- GROUP BY month, country
+FROM transactions
+GROUP BY month, country
 
 -- Another solution (Used FILTER and COALESCE)
 -- This solution is really fast (450ms compare to 750ms of mine)
