b8f7592ce3ee9f948e7ce09913b9a3d2b268c6a3
Trí-Anh Nguyễn <78487345+AubrynnReina@users.noreply.github.com>
2024-08-29 15:15:00 +0700

    Time: 525 ms (79.04%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,15 +1,33 @@
+-- -- My solution (Used Window Function)
+-- SELECT
+--     ROUND(
+--         CAST(COUNT(DISTINCT(player_id)) AS DECIMAL) / (
+--             SELECT COUNT(DISTINCT(player_id))
+--             FROM Activity
+--         ), 2
+--     ) AS fraction
+-- FROM (
+--     SELECT 
+--         player_id,
+--         event_date,
+--         MIN(event_date) OVER(PARTITION BY player_id) first_event
+--     FROM Activity
+-- )
+-- WHERE event_date - first_event = 1
+
+-- -- Another solution (Used WHERE - IN)
 SELECT
     ROUND(
-        CAST(COUNT(DISTINCT(player_id)) AS DECIMAL) / (
+        CAST(COUNT(*) AS DECIMAL) / (
             SELECT COUNT(DISTINCT(player_id))
             FROM Activity
         ), 2
     ) AS fraction
-FROM (
+FROM Activity
+WHERE (player_id, event_date) IN (
     SELECT 
         player_id,
-        event_date,
-        MIN(event_date) OVER(PARTITION BY player_id) first_event
-    FROM activity
+        MIN(event_date) + INTERVAL '1 day' AS day_after_first
+    FROM Activity
+    GROUP BY player_id
 )
-WHERE event_date - first_event = 1
