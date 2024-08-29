b1444e9fa5d26c8ae0a50b2f76af8dbd0699ac1e
Trí-Anh Nguyễn <78487345+AubrynnReina@users.noreply.github.com>
2024-08-29 15:15:37 +0700

    Update game-play-analysis-iv.sql

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,21 +1,21 @@
--- -- My solution (Used Window Function)
--- SELECT
---     ROUND(
---         CAST(COUNT(DISTINCT(player_id)) AS DECIMAL) / (
---             SELECT COUNT(DISTINCT(player_id))
---             FROM Activity
---         ), 2
---     ) AS fraction
--- FROM (
---     SELECT 
---         player_id,
---         event_date,
---         MIN(event_date) OVER(PARTITION BY player_id) first_event
---     FROM Activity
--- )
--- WHERE event_date - first_event = 1
+-- My solution (Used Window Function)
+SELECT
+    ROUND(
+        CAST(COUNT(DISTINCT(player_id)) AS DECIMAL) / (
+            SELECT COUNT(DISTINCT(player_id))
+            FROM Activity
+        ), 2
+    ) AS fraction
+FROM (
+    SELECT 
+        player_id,
+        event_date,
+        MIN(event_date) OVER(PARTITION BY player_id) first_event
+    FROM Activity
+)
+WHERE event_date - first_event = 1
 
--- -- Another solution (Used WHERE - IN)
+-- Another solution (Used WHERE - IN)
 SELECT
     ROUND(
         CAST(COUNT(*) AS DECIMAL) / (
