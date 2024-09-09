e49cad7a8f6f5f7911adee3f5b593763dcc18116
Trí-Anh Nguyễn <78487345+AubrynnReina@users.noreply.github.com>
2024-09-09 16:44:53 +0700

    Time: 274 ms (65.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,16 @@
+-- Write your PostgreSQL query statement below
+select round(cast(sum(tiv_2016) as numeric), 2) as tiv_2016
+from insurance
+where 
+    tiv_2015 not in (
+        select tiv_2015
+        from insurance
+        group by tiv_2015
+        having count(*) = 1
+    ) and
+    (lat, lon) not in (
+        select lat, lon
+        from insurance
+        group by (lat, lon)
+        having count(*) > 1
+    )
