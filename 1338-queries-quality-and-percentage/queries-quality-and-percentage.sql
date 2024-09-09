e2c824d426da223379ad88a902f25c0d23f72f77
Trí-Anh Nguyễn <78487345+AubrynnReina@users.noreply.github.com>
2024-09-09 16:09:11 +0700

    Time: 263 ms (47.62%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -4,5 +4,5 @@ select
     round(avg(rating * 1.0 / position), 2) as quality,
     round(avg(case when rating < 3 then 1 else 0 end) * 100, 2) as poor_query_percentage
 from queries
+where query_name is not NULL
 group by query_name
-having query_name is not NULL
