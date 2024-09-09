e589ad028f06a4c1a39995c4d54efcc86a321392
Trí-Anh Nguyễn <78487345+AubrynnReina@users.noreply.github.com>
2024-09-09 16:08:09 +0700

    Time: 240 ms (68.97%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,8 +1,8 @@
 -- Write your PostgreSQL query statement below
 select
     query_name,
-    round(sum(rating * 1.0 / position) / count(*), 2) as quality,
-    round(sum(case when rating < 3 then 1 else 0 end) * 100.0 / count(*), 2) as poor_query_percentage
+    round(avg(rating * 1.0 / position), 2) as quality,
+    round(avg(case when rating < 3 then 1 else 0 end) * 100, 2) as poor_query_percentage
 from queries
 where query_name is not NULL
 group by query_name
